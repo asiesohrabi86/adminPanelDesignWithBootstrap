784bd436af0c1fceabe65dc3d7a7018a3d9d06f3
asiesohrabi <asie.sohrabiii@gmail.com>
2025-11-06 23:39:04 +0330

    duplicate all files & golders to the git

---
+++ assets/js/grid.js
@@ -0,0 +1,130 @@
+$(document).ready(function(){
+
+    function removeAllSidebarToggleClasses() {
+        $('#sidebar-toggle-hide').removeClass('d-md-inline');
+        $('#sidebar-toggle-hide').removeClass('d-none');
+        $('#sidebar-toggle-show').removeClass('d-inline');
+        $('#sidebar-toggle-show').removeClass('d-md-none');
+    }
+
+    $('#sidebar-toggle-hide').click(function(){
+        // $('#sidebar').fadeToggle(300);
+        $('#sidebar').fadeOut(300);
+        $('#main-body').animate({width: "100%"},300);
+
+        setTimeout(function() {
+            removeAllSidebarToggleClasses();
+            $('#sidebar-toggle-hide').addClass('d-none');
+            $('#sidebar-toggle-show').removeClass('d-none');
+        }, 300);
+    });
+
+    $('#sidebar-toggle-show').click(function(){
+        $('#sidebar').fadeIn(300);
+        // $('#main-body').animate({width:"calc(100% - 14rem)"}, 300);
+
+        setTimeout(function(){
+            // removeAllSidebarToggleClasses();
+            $('#sidebar-toggle-hide').removeClass('d-none');
+            $('#sidebar-toggle-show').addClass('d-none');
+        },300);
+    });
+
+    $('#menu-toggle').click(function(){
+        $('#body-header').toggle(300);
+    });
+
+    $('#search-toggle').click(function(){
+        $('#search-toggle').removeClass('d-md-inline');
+        $('#search-input').animate({width: "12rem"}, 300);
+        $('#search-area').addClass('d-md-inline');
+    });
+
+    $('#search-area-hide').click(function(){
+        $('#search-input').animate({width: "0"}, 300);
+        setTimeout(function(){
+            $('#search-toggle').addClass('d-md-inline');
+            $('#search-area').removeClass('d-md-inline'); 
+        }, 300);
+        
+       
+    });
+
+    $('#header-notification-toggle').click(function(){
+        $('#header-notification').fadeToggle();
+    });
+
+    $('#header-comment-toggle').click(function(){
+        $('#header-comment').fadeToggle();
+    });
+
+    $('#header-profile-toggle').click(function(){
+        $('#header-profile').fadeToggle();
+    });
+
+    $('.sidebar-group-link').click(function(){
+        if($(this).hasClass("sidebar-group-link-active")){
+            var status = 1;
+        }else{
+            status = 0;
+        }
+
+        $('.sidebar-group-link').removeClass('sidebar-group-link-active');
+        $('.sidebar-group-link').children('.sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-down');
+        $('.sidebar-group-link').children('.sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-left');
+    
+        if(status === 0){
+            $(this).addClass('sidebar-group-link-active');
+            $(this).children('.sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-left');
+            $(this).children('.sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-down');
+        }
+                    
+    });
+
+
+    // بخش full screen
+    $('#full-screen').click(function(){
+        toggleFullScreen();
+    });
+
+    function toggleFullScreen(){
+        if((document.fullScreenElement && fullScreenElement != null) || (! document.mozFullSceen && ! document.webkitIsFullScreen)){
+            if(document.documentElement.requestFullScreen){
+                document.documentElement.requestFullScreen();
+            }
+
+            else if(document.documentElement.mozFullSceen){
+                document.documentElement.mozRequestFullScreen();
+            }
+
+            else if(document.documentElement.webkitRequestFullScreen){
+                document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
+
+            $('#screen-compress').removeClass('d-none');
+            $('#screen-expand').addClass('d-none');
+        }
+
+        else{
+
+            if (document.cancelFullScreen) {
+                
+                document.cancelFullScreen();
+            }
+
+            else if (document.moazCancelFullScreen) {
+                
+                document.moazCancelFullScreen();
+            }
+
+            else if (document.webkitCancelFullScreen) {
+                
+                document.webkitCancelFullScreen();
+            }
+
+            $('#screen-compress').addClass('d-none');
+            $('#screen-expand').removeClass('d-none');
+        }
+
+    }
+});
